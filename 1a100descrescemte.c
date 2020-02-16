332d8a64856a16efd6b40f4cb2e6a5ca2aff7b6e
Viviane Farias <viviianefarias@hotmail.com>
2020-02-15 23:10:28 -0300

    Commit inicial

---
+++ 1a100descrescemte.c
@@ -0,0 +1,12 @@
+// Faça um programa em que imprima os numeros de 1 a 100 em ordem decrescente.
+
+#include<stdio.h>
+
+int main ()
+{
+	int numero;
+	for(numero=100;numero>=1;numero--)
+	{
+	   printf("\n %d",numero);
+	}
+}
---
+++ DiasVividos.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    char nome [100]	;
+    
+       int idade ,r;
+    
+    printf( "\n informe seu nome: " );
+       scanf ( "%s",nome);
+    printf ("\n informe sua idade: ");
+       scanf ("%d", & idade);
+       r= (idade*365);
+       printf ("\n %s, voce viveu aproximadamente %d dias", nome, r);
+       
+	
+	
+	
+
+	
+	
+}
